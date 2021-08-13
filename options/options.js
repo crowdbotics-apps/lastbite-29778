af064d822307f352731a2bac25a46f8be285c49e
Crowdbotics <team@crowdbotics.com>
2021-08-13 11:14:36 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "lastbite_29778Identifier",
+  url: "https://lastbite_29778Identifier.botics.co",
+  api: "https://lastbite_29778Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
